420dd1680fe23cb73d3a35d60f7e77fd89512bac
Ross Hemsley <ross@citymapper.com>
2017-02-26 10:06:23 +0000

    Change cmake version

--- mnist/mnist.h
+++ mnist/mnist.h
@@ -14,7 +14,8 @@ using Label = uint8_t;
 /**
  * @brief       Load MNIST data from a stream
  *
- * Data is available at http://yann.lecun.com/exdb/mnist/
+ * Data is available at http://yann.lecun.com/exdb/mnist
+ * Reads binary files in the following form
  *
  * [offset] [type]          [value]          [description]
  * 0000     32 bit integer  0x00000803(2051) magic number
@@ -30,6 +31,10 @@ using Label = uint8_t;
 std::vector<Image> load_images(std::istream&);
 
 /**
+ * @brief      Load MNIST labels data from a stream
+ *
+ * Reads binary files in the following form
+ *
  * [offset] [type]          [value]          [description]
  * 0000     32 bit integer  0x00000801(2049) magic number (MSB first)
  * 0004     32 bit integer  10000            number of items
@@ -44,4 +49,5 @@ std::vector<Label> load_labels(std::istream&);
  * @brief      Write a bmp file to a stream from a loaded image.
  */
 void write_image_bmp(const Image&, std::ostream&);
-};
+
+} // namespace mnist
